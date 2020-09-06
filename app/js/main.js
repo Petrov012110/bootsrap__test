4375fe5f324d5b93cdbc6ca5d7f24be37008605f
Petrov012110 <grisha14.06.96@gmail.com>
2020-09-06 19:48:38 +0500

    project was done

--- app/js/main.js
+++ app/js/main.js
@@ -16,4 +16,16 @@ $(function(){
             return '<button> </button>';
             },
     });
+
+    $('.menu__logo-btn__burger').on('click', function(){
+        $('.menu__list').slideToggle();
+        $('.global-wrapper').toggleClass('shadows');
+        $('.menu__logo-btn__burger').toggleClass('on');
+    });
+
+    $('.header__content-search').on('click', function(){
+        $('.header__search-form').toggleClass('active');
+        $('.header__logo').toggleClass('hidden');
+    });
+
 });
